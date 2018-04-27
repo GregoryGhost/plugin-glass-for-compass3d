99d5c399c31a100091239a29a0dc31494ed3074e
Gregory <lobster287@gmail.com>
2018-04-28 04:55:21 +0700

    Added project for testing glass model on the NUnit framework

---
+++ GlassModel.Tests/NUnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GlassModel.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void ThisWork()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
