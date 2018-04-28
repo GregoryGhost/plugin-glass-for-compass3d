951634920b28b228695c9273f3a87929b1a25c4b
Gregory <lobster287@gmail.com>
2018-04-29 03:00:37 +0700

    Added test for fixing params of clean glass

---
+++ GlassModel.Tests/CleanGlassTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using GlassModel;
+using System;
+
+
+namespace GlassModel.Tests
+{
+    [TestFixture]
+    public class CleanGlassTest
+    {
+        private CleanGlass _cleanGlass;
+        private const double _angle = 0.0f;
+        private const double _heightFaceted = 0.0f;
+        private const int _countFaceted = 0;
+        private const double min = 10;
+        private const double max = 200;
+
+        [SetUp]
+        public void Setup()
+        {
+            var height = new BorderConditions<double>(min, min, max);
+            var diameterBottom = new BorderConditions<double>(min / 2,
+                min / 2, max / 2);
+
+            _cleanGlass = new CleanGlass(diameterBottom, height);
+        }
+
+        [Test(Description = "Check fixed params of clean glass -" +
+            "angle height, count faceted, height faceted")]
+        [TestCase(_angle, _heightFaceted, _countFaceted,
+            TestName = "Fixed params of clean glass")]
+        public void CheckFixedParams(double angle, double hFaceted,
+            int kFaceted)
+        {
+            _cleanGlass.AngleHeight = angle + 10;
+            _cleanGlass.HeightFaceted = hFaceted + 10;
+            _cleanGlass.CountFaceted = kFaceted + 10;
+
+            Assert.That(angle, Is.EqualTo(_cleanGlass.AngleHeight));
+            Assert.That(hFaceted, Is.EqualTo(_cleanGlass.HeightFaceted));
+            Assert.That(kFaceted, Is.EqualTo(_cleanGlass.CountFaceted));
+        }
+
+        //[Test(Description="Setting the correct data for the parameters")]
+        //[TestCase()]
+        //public void SettingCorrectDataForParamsPositive()
+        //{
+        //    _cleanGlass.DiameterBottom = diameter;          
+        //    _cleanGlass.Height = height;
+        //    _cleanGlass.DepthSide = dSide;
+        //    _cleanGlass.DepthBottom = dBottom;
+
+
+        //}
+    }
+}
---
+++ GlassModel/CleanGlass.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassModel
+{
+    /// <summary>
+    /// Гладкий стакан.
+    /// </summary>
+    public class CleanGlass : IGlass, IChecker
+    {
+        /// <summary>
+        /// Зависимые и фиксированные параметры стакана
+        /// </summary>
+        private IAutoCalcParams _dependencies;
+
+        /// <summary>
+        /// Постоянный угол наклона высоты стакана
+        /// </summary>
+        private readonly double _angleHeight = 0.0f;
+
+        /// <summary>
+        /// Отсутствует узор стакана => высота узора равна нулю 
+        /// </summary>
+        private readonly double _heightFaceted = 0.0f;
+
+        /// <summary>
+        /// Отсутствуют грани стакана => количество граней равно нулю
+        /// </summary>
+        private readonly int _countFaceted = 0;
+
+        /// <summary>
+        /// Установление параметров гладкого стакана.
+        /// </summary>
+        /// <param name="diameterBottom">Диаметр дна стакана.</param>
+        /// <param name="height">Высота стакана.</param>
+        public CleanGlass(BorderConditions<double> diameterBottom,
+            BorderConditions<double> height)
+        {
+            _dependencies = new DependenciesParams();
+            _dependencies.AngleHeight = true;
+            _dependencies.CountFaceted = true;
+            _dependencies.DepthBottom = true;
+            _dependencies.DepthSide = true;
+            _dependencies.HeightFaceted = true;
+        }
+
+        /// <summary>
+        /// Высота стакана
+        /// </summary>
+        public double Height
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Диаметр дна стакана
+        /// </summary>
+        public double DiameterBottom
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Отсутствующий угол наклона высоты стакана
+        /// </summary>
+        public double AngleHeight
+        {
+            get
+            {
+                return _angleHeight;
+            }
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Зависящий параметр - толщина стенки стакана
+        /// </summary>
+        public double DepthSide
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Зависящий параметр - толщина дна
+        /// </summary>
+        public double DepthBottom
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Отсутствующий узор
+        /// </summary>
+        public double HeightFaceted
+        {
+            get
+            {
+                return _heightFaceted;
+            }
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Отсутствующие грани
+        /// </summary>
+        public int CountFaceted
+        {
+            get
+            {
+                return _countFaceted;
+            }
+            set
+            {
+            }
+        }
+
+        public bool IsValid
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
