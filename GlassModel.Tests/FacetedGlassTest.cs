83ad5dff0ac28ac35785c6ceaea340a1d95df2fe
Gregory <lobster287@gmail.com>
2018-04-30 03:06:03 +0700

    Added test for height glass - positive

--- GlassModel.Tests/FacetedGlassTest.cs
+++ GlassModel.Tests/FacetedGlassTest.cs
@@ -15,6 +15,13 @@ namespace GlassModel.Tests
 
         private const double _min = 10;
         private const double _max = 200;
+        private const double _minAngle = 0;
+        private const double _maxAngle = 5;
+        private const double _minDepth = 1;
+        private const double _maxDepthSide = 5;
+        private const double _maxDepthBottom = 7;
+        private const int _minCountFaceted = 4;
+        private const int _maxCountFaceted = 20;
 
         private const double _percentForDepthBottom = 7;
         private const double _percentForHeightFaceted = 90;
@@ -23,18 +30,29 @@ namespace GlassModel.Tests
         private const bool _valid = true;
         private const bool _invalid = false;
 
+
         [SetUp]
         public void Setup()
         {
             var height = new BorderConditions<double>(_min, _min, _max);
             var diameterBottom = new BorderConditions<double>(_min / 2,
                 _min / 2, _max / 2);
-            _facetedGlass = new FacetedGlass(height, diameterBottom);
+            var angleHeight = new BorderConditions<double>(_minAngle,
+                _minAngle, _maxAngle);
+            var depthSide = new BorderConditions<double>(_minDepth,
+                _minDepth, _maxDepthSide);
+            var depthBottom = new BorderConditions<double>(_minDepth,
+                _minDepth, _maxDepthBottom);
+            var countFaceted = new BorderConditions<int>(
+                _minCountFaceted, _minCountFaceted, _maxCountFaceted);
+
+            _facetedGlass = new FacetedGlass(height, diameterBottom,
+                angleHeight, depthSide, depthBottom, countFaceted);
         }
 
-        [Test(Description = "Check dependencies params of faceted glass" + 
+        [Test(Description = "Check dependencies params of faceted glass" +
             " - height faceted")]
-        [TestCase(_min, TestName="Depend params - Height glass = min")]
+        [TestCase(_min, TestName = "Depend params - Height glass = min")]
         [TestCase(_max, TestName = "Depend params - Height glass = max")]
         [TestCase((_min + _max) / 2,
             TestName = "Depend params - Height glass = average")]
@@ -42,7 +60,7 @@ namespace GlassModel.Tests
         {
             _facetedGlass.Height = height;
 
-            var expHeightFaceted = height * 
+            var expHeightFaceted = height *
                 _percentForHeightFaceted / 100;
 
             Assert.AreEqual(expHeightFaceted,
@@ -51,26 +69,32 @@ namespace GlassModel.Tests
 
         [Test(Description = "Check setted params of faceted glass -" +
             "height, diameter bottom - correct data")]
-        [TestCase(_min, _min / 2, _valid,
-            TestName = "Setted - Height glass = min, diameter bottom = min")]
-        [TestCase(_max, _max / 2, _valid,
-            TestName = "Setted - Height glass = max, diameter bottom = max")]
-        [TestCase((_min + _max) / 2, (_min / 2 + _max / 2) / 2, _valid,
-            TestName = "Setted - Height glass and diameter bottom" +
-                "in the allowable range")]
-        [TestCase(_max, _min / 2, _valid,
+        [TestCase(_min, _min / 2, _minAngle, _valid,
+            TestName = "Setted - Height glass = min," +
+                "diameter bottom = min, angle height = min")]
+        [TestCase(_max, _max / 2, _maxAngle, _valid,
+            TestName = "Setted - Height glass = max," +
+                "diameter bottom = max, angle height = max")]
+        [TestCase((_min + _max) / 2, (_min / 2 + _max / 2) / 2, _minAngle,
+            _valid, TestName = "Setted - Height glass and diameter bottom" +
+                    "in the allowable range")]
+        [TestCase(_max, _min / 2, _minAngle, _valid,
             TestName = "Setted - Height glass >= diameter bottom,")]
         public void CheckSettedParamsPositive(double height,
-            double diameterBottom, bool expIsValid)
+            double diameterBottom, double angleHeight, bool expIsValid)
         {
             _facetedGlass.Height = height;
             _facetedGlass.DiameterBottom = diameterBottom;
-
+            //height, diameter bottom
             Assert.That(height, Is.EqualTo(_facetedGlass.Height));
-            Assert.That(diameterBottom, 
+            Assert.That(diameterBottom,
                 Is.EqualTo(_facetedGlass.DiameterBottom));
             Assert.That(
                 _facetedGlass.Height >= _facetedGlass.DiameterBottom);
+
+            _facetedGlass.AngleHeight = angleHeight;
+            //angleHeight, depthSide, depthBottom, countFaceted
+            Assert.That(angleHeight, Is.EqualTo(_facetedGlass.AngleHeight));
 
             Assert.That(expIsValid,
                 Is.EqualTo(_facetedGlass.IsValid));
