c3fce4d679ae033e8656295a49467bb1e4cc6653
Gregory <lobster287@gmail.com>
2018-04-30 23:48:48 +0700

    Added completed tests for setted params of faceted glass

--- GlassModel.Tests/FacetedGlassTest.cs
+++ GlassModel.Tests/FacetedGlassTest.cs
@@ -68,8 +68,9 @@ namespace GlassModel.Tests
         }
 
         [Test(Description = "Check setted params of faceted glass -" +
-            "height, diameter bottom - correct data")]
-        [TestCase(_min, _min / 2, _minAngle, _minDepth, 
+            "height, diameter bottom, angle height," +
+            "depth side and bottom, also count faceted - correct data")]
+        [TestCase(_min, _min / 2, _minAngle, _minDepth,
             _minDepth, _minCountFaceted, _valid,
                 TestName = "Setted - All params = min")]
         [TestCase(_max, _max / 2, _maxAngle, _maxDepthSide,
@@ -110,6 +111,70 @@ namespace GlassModel.Tests
             Assert.That(expDepthSide, Is.EqualTo(_facetedGlass.DepthSide));
             Assert.That(expDepthBottom, Is.EqualTo(_facetedGlass.DepthBottom));
             Assert.That(countFaceted, Is.EqualTo(_facetedGlass.CountFaceted));
+
+            Assert.That(expIsValid,
+                Is.EqualTo(_facetedGlass.IsValid));
+        }
+
+        [Test(Description = "Check all setted params of faceted glass - " +
+            "incorrect data")]
+        [TestCase(_min / 10, _min / 2, _minAngle, _minDepth,
+            _minDepth, _minCountFaceted, _invalid,
+                TestName = "Setted neg - height < min")]
+        [TestCase(_max * 2, _max / 2, _maxAngle, _maxDepthSide,
+            _maxDepthBottom, _maxCountFaceted, _invalid,
+                TestName = "Setted neg - height > max")]
+        [TestCase(_min, _min / 10, _minAngle, _minDepth,
+            _minDepth, _minCountFaceted, _invalid,
+                TestName = "Setted neg - diameter bottom < min")]
+        [TestCase(_min, _max, _minAngle, _minDepth,
+            _minDepth, _minCountFaceted, _invalid,
+                TestName = "Setted neg - diameter bottom > max")]
+        [TestCase(_min, _max, _minAngle /2 , _minDepth,
+            _minDepth, _minCountFaceted, _invalid,
+                TestName = "Setted neg - angle height < min")]
+        [TestCase(_max, _min, _maxAngle * 2, _minDepth,
+            _minDepth, _minCountFaceted, _invalid,
+                TestName = "Setted neg - angle height > max")]
+        [TestCase(_max, _min, _maxAngle, _minDepth / 10,
+            _minDepth, _minCountFaceted, _invalid,
+                TestName = "Setted neg - depth side < min")]
+        [TestCase(_max, _min, _maxAngle, _maxDepthSide * 2,
+            _minDepth, _minCountFaceted, _invalid,
+                TestName = "Setted neg - depth side > max")]
+        [TestCase(_max, _min, _maxAngle, _minDepth,
+            _minDepth / 10, _minCountFaceted, _invalid,
+                TestName = "Setted neg - depth bottom < min")]
+        [TestCase(_max, _min, _maxAngle, _maxDepthSide,
+            _maxDepthBottom * 2, _minCountFaceted, _invalid,
+                TestName = "Setted neg - depth bottom > max")]
+        [TestCase(_max, _min, _maxAngle, _minDepth,
+            _minDepth, _minCountFaceted - 100, _invalid,
+                TestName = "Setted neg - count faceted < min")]
+        [TestCase(_max, _min, _maxAngle, _maxDepthSide,
+            _maxDepthBottom, _minCountFaceted + 100, _invalid,
+                TestName = "Setted neg - count faceted > max")]
+        [TestCase(_min, _max, _minAngle, _maxDepthSide,
+            _maxDepthBottom, _maxCountFaceted, _invalid,
+                TestName = "Setted neg - Height glass < diameter bottom,")]
+        public void CheckSettedParamsNegative(double height,
+            double diameterBottom, double angleHeight, double depthSide,
+                double depthBottom, int countFaceted, bool expIsValid)
+        {
+            var expDepthSide = CleanGlassTest.CalcDepthOfGlass(
+                diameterBottom, depthSide);
+            var expDepthBottom = CleanGlassTest.CalcDepthOfGlass(
+                height, depthBottom);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _facetedGlass.Height = height;
+                _facetedGlass.DiameterBottom = diameterBottom;
+                _facetedGlass.AngleHeight = angleHeight;
+                _facetedGlass.DepthSide = depthSide;
+                _facetedGlass.DepthBottom = depthBottom;
+                _facetedGlass.CountFaceted = countFaceted;
+            });
 
             Assert.That(expIsValid,
                 Is.EqualTo(_facetedGlass.IsValid));
