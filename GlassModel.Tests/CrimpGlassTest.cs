6a8b32b7e7c091993eeab538a12c41b5da628872
Gregory <lobster287@gmail.com>
2018-05-01 05:42:48 +0700

    Added test for setted params of crimp glass

--- GlassModel.Tests/CrimpGlassTest.cs
+++ GlassModel.Tests/CrimpGlassTest.cs
@@ -74,5 +74,80 @@ namespace GlassModel.Tests
             Assert.That(expDepthSide, Is.EqualTo(_crimpGlass.DepthSide));
             Assert.That(expDepthBottom, Is.EqualTo(_crimpGlass.DepthBottom));
         }
+
+
+        [Test(Description = "Check setted params of crimp glass -" +
+            "height, diameter bottom - correct data")]
+        [TestCase(_min, _min / 2, _minCountFaceted, _valid,
+            TestName = "Setted - Height glass = min, diameter bottom = min")]
+        [TestCase(_max, _max / 2, _maxCountFaceted, _valid,
+            TestName = "Setted - Height glass = max, diameter bottom = max")]
+        [TestCase((_min + _max) / 2, (_min / 2 + _max / 2) / 2,
+            (_minCountFaceted + _maxCountFaceted) / 2, _valid,
+                TestName = "Setted - Height glass and diameter bottom" +
+                    "in the allowable range")]
+        [TestCase(_max, _min / 2, _minCountFaceted, _valid,
+            TestName = "Setted - Height glass >= diameter bottom,")]
+        public void CheckSettedParamsPositive(double height,
+            double diameterBottom, int countFaceted, bool expIsValid)
+        {
+            _crimpGlass.Height = height;
+            _crimpGlass.DiameterBottom = diameterBottom;
+            _crimpGlass.CountFaceted = countFaceted;
+
+            Assert.That(height, Is.EqualTo(_crimpGlass.Height));
+            Assert.That(diameterBottom,
+                Is.EqualTo(_crimpGlass.DiameterBottom));
+            Assert.That(_crimpGlass.Height >= _crimpGlass.DiameterBottom);
+            Assert.That(countFaceted, Is.EqualTo(_crimpGlass.CountFaceted));
+
+            Assert.That(expIsValid, Is.EqualTo(_crimpGlass.IsValid));
+        }
+
+        [Test(Description = "Check setted params of crimp glass -" +
+            "height, diameter bottom - incorrect data")]
+        [TestCase(_min / 10, _min / 2, _invalid,
+            TestName = "Setted neg - Height glass < min, diameter bottom = min")]
+        [TestCase(_min / 10, _min / 2, _invalid,
+            TestName = "Setted neg - Height glass = min, diameter bottom < min")]
+        [TestCase(_max * 2, _max / 2, _invalid,
+            TestName = "Setted neg - Height glass > max, diameter bottom = max")]
+        [TestCase(_max, _max, _invalid,
+            TestName = "Setted neg - Height glass = max, diameter bottom > max")]
+        [TestCase(_min, _max / 2, _invalid,
+            TestName = "Setted neg - Height glass < diameter bottom,")]
+        public void CheckSettedParamsNegative(double height,
+            double diameterBottom, bool expIsValid)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _crimpGlass.Height = height;
+                _crimpGlass.DiameterBottom = diameterBottom;
+            });
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _crimpGlass.DiameterBottom = diameterBottom;
+                _crimpGlass.Height = height;
+            });
+
+            Assert.That(expIsValid, Is.EqualTo(_crimpGlass.IsValid));
+        }
+
+        [Test(Description = "Check setted params of crimp glass -" +
+            "count faceted - incorrect data")]
+        [TestCase(_minCountFaceted / 2, _invalid,
+            TestName="Setted neg - count faceted < min")]
+        [TestCase(_maxCountFaceted * 2, _invalid,
+            TestName = "Setted neg - count faceted > max")]
+        public void CheckSettedParamsNegative2(int countFaceted,
+            bool expIsValid)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _crimpGlass.CountFaceted = countFaceted;
+            });
+
+            Assert.That(expIsValid, Is.EqualTo(_crimpGlass.IsValid));
+        }
     }
 }
