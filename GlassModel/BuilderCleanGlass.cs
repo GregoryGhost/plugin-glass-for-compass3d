2f4100c93833840bd6f9b99f81390c7e66ebe4d1
Gregory <lobster287@gmail.com>
2018-05-06 01:55:11 +0700

    Added builder of clean glass

---
+++ GlassModel/BuilderCleanGlass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassModel
+{
+    public class BuilderCleanGlass : IBuilder
+    {
+        private IBuilder _builderBlank;
+
+        private KompasWrapper _kompas;
+
+        public BuilderCleanGlass()
+        {
+            _kompas = KompasWrapper.Instance;
+
+            _builderBlank = new BuilderOfBlank();
+        }
+
+        public void Build(IGlass glass, IChecker checker)
+        {
+            _builderBlank.Build(glass, checker);
+        }
+    }
+}
