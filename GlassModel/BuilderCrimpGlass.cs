aa4573cb4fa50660fef507f65965dd1c0f6cf59b
Gregory <lobster287@gmail.com>
2018-05-06 02:40:32 +0700

    Added template version builder of crimp glass

---
+++ GlassModel/BuilderCrimpGlass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassModel
+{
+    public class BuilderCrimpGlass : IBuilder
+    {
+        private IBuilder _builderBlank;
+
+        private KompasWrapper _kompas;
+
+        public BuilderCrimpGlass()
+        {
+            _kompas = KompasWrapper.Instance;
+
+            _builderBlank = new BuilderOfBlank();
+        }
+
+        public void Build(IGlass glass, IChecker checker)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
