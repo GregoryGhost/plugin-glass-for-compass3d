e467b81ca8ed40d1b81f4e12c4d9b58a0f93f978
Gregory <lobster287@gmail.com>
2018-05-09 01:15:51 +0700

    Added xml comments for members of builders clean and crimp glasses.

--- GlassModel/BuilderCleanGlass.cs
+++ GlassModel/BuilderCleanGlass.cs
@@ -6,12 +6,24 @@ using System.Threading.Tasks;
 
 namespace GlassModel
 {
+    /// <summary>
+    /// Построитель гладкий стаканов.
+    /// </summary>
     public class BuilderCleanGlass : IBuilder
     {
+        /// <summary>
+        /// Построитель болванок стакана.
+        /// </summary>
         private IBuilder _builderBlank;
 
+        /// <summary>
+        /// Экземпляр Компаса 3D.
+        /// </summary>
         private KompasWrapper _kompas;
 
+        /// <summary>
+        /// Установление связи с Компасом 3D.
+        /// </summary>
         public BuilderCleanGlass()
         {
             _kompas = KompasWrapper.Instance;
@@ -19,6 +31,12 @@ namespace GlassModel
             _builderBlank = new BuilderOfBlank();
         }
 
+        /// <summary>
+        /// Построить гладкий стакан.
+        /// </summary>
+        /// <param name="glass">Гладкий стакан.</param>
+        /// <param name="checker">Проверяющий параметры стакана
+        ///     в соответствие с требованиям предметной области.</param>
         public void Build(IGlass glass, IChecker checker)
         {
             _builderBlank.Build(glass, checker);
