99cc6f88cf4e435eb753bde6a6a19ced6555c9df
Gregory <lobster287@gmail.com>
2018-05-16 01:11:00 +0700

    Added enter max count building glasses

--- LoadingPlugin.Tests/Program.fs
+++ LoadingPlugin.Tests/Program.fs
@@ -1,8 +1,19 @@
 ﻿open FSharp.Charting
 open LoadingPlugin.Tests.BuildingGlassesTest
+open System
 
 [<EntryPoint>]
 let main argv = 
-    let charts = timeBuild |> Chart.Combine
+    let maxCountBuilding = 
+        printfn "Enter max count building of glasses:"
+        let k = Console.ReadLine() |> int
+        Console.ForegroundColor <- ConsoleColor.Green
+        printfn "Ok: %d" k
+        Console.ForegroundColor <- ConsoleColor.White
+        k
+    let charts = 
+        maxCountBuilding
+        |> timeBuild 
+        |> Chart.Combine
     charts |> Chart.Show
     0
