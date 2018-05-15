c91c10c4a6258043c76797b26def991123072a88
Gregory <lobster287@gmail.com>
2018-05-16 00:15:05 +0700

    Fixing: references on the view model in the view project

--- GlassPlugin/MainWindow.xaml.cs
+++ GlassPlugin/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using GlassModel;
 using GlassViewModel;
 
 namespace GlassPlugin
