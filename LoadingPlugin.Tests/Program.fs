766266eb7a9d6144b03294b1d499ab10b40aca79
Gregory <lobster287@gmail.com>
2018-05-16 00:49:01 +0700

    Refactor - extract code in the module for loading plugin tests project, also setted max values for glasses in the view model

--- LoadingPlugin.Tests/Program.fs
+++ LoadingPlugin.Tests/Program.fs
@@ -1,44 +1,8 @@
 ﻿open FSharp.Charting
-open GlassViewsModel
-open System.Diagnostics
-open System
+open LoadingPlugin.Tests.BuildingGlassesTest
 
 [<EntryPoint>]
 let main argv = 
-    let maxCountBuilding = 1
-    let sw = new Stopwatch()
-    let glassesVM = new GlassesViewModel()
-
-    let glasses = [
-        for i in 0..glassesVM.CountGlasses-1 ->
-            glassesVM.GetGlassByIndex(i) ]
-    
-    let measureTime (glass : GlassViewModel, maxCount : int) = [
-        for x in 0..maxCount ->
-            sw.Start()
-            glass.BuildModel() |> ignore
-            sw.Stop()
-            let mls = 
-                sw.ElapsedMilliseconds |> double
-            let timeBuilding = 
-                mls / 1000.
-            (x, timeBuilding)    ]
-    
-    let calcTimeBuild glass = 
-        let time =
-            measureTime(glass, maxCountBuilding)
-        let name = glass.Name
-        (time, name)
-
-    let genChart(glass : (int * double) list * string) =
-        let (data, name) = glass
-        Chart.Line (data, Name = name)
-
-    let timeBuild =
-        glasses
-        |> List.map calcTimeBuild
-        |> List.map genChart
-
     let charts = timeBuild |> Chart.Combine
     charts |> Chart.Show
     0
