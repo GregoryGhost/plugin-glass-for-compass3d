d6aaf21ee28a7af3fa4709450fa8d1f18e19c1d0
Gregory <lobster287@gmail.com>
2018-05-23 02:30:40 +0700

    Fixing: chart of building glasses in the CAD - loading tests plugin

--- LoadingPlugin.Tests/BuildingGlassesTest.fs
+++ LoadingPlugin.Tests/BuildingGlassesTest.fs
@@ -11,10 +11,12 @@ module BuildingGlassesTest =
 
     let glasses = [
         for i in 0..glassesVM.CountGlasses-1 ->
-            glassesVM.GetGlassByIndex(i) ]
+            let g = glassesVM.GetGlassByIndex(i)
+            g.Filleted <- true
+            g ]
     
     let measureTime (glass : GlassViewModel, maxCount : int) = [
-        for x in 0..maxCount ->
+        for x in 1..maxCount ->
             sw.Start()
             glass.BuildModel() |> ignore
             sw.Stop()
@@ -22,6 +24,8 @@ module BuildingGlassesTest =
                 sw.ElapsedMilliseconds |> double
             let timeBuilding = 
                 mls / 1000.
+            sw.Reset()
+            printfn "Building %s glass №%d" glass.Name x
             (x, timeBuilding)    ]
     
     let calcTimeBuild glass n = 
@@ -33,10 +37,50 @@ module BuildingGlassesTest =
 
     let genChart(glass : (int * double) list * string) =
         let (data, name) = glass
-        Chart.Line (data, Name = name)
+        Chart.Line(data, Name = name)
 
     let timeBuild n =
         let calc g = calcTimeBuild g n
-        glasses
-        |> List.map calc
-        |> List.map genChart
+        let times = 
+            glasses |> List.map calc
+        times
+
+    let timeBuilding(times : ((int * double) list * string) list) = 
+        times
+        |> List.map (fun (points, name) -> 
+            let _, y = points |> List.unzip
+            let y1 = List.sum y
+            (y1, name)   )
+
+    let chartsTimeBuilding times = 
+        times |> List.map genChart
+
+    let printSummary s =
+        Console.ForegroundColor <- ConsoleColor.Green
+        printfn "Summary time building of glasses: %f (sec)" s
+        Console.ForegroundColor <- ConsoleColor.White
+
+    let printForEachGlass timeBuilding =
+            timeBuilding
+            |> List.iter (fun (t, n) -> 
+                Console.ForegroundColor <- ConsoleColor.Yellow
+                printfn "Time building %s glass: %f (sec)" n t
+                Console.ForegroundColor <- ConsoleColor.White )
+     
+    let chartsBuilding tb = 
+        tb 
+        |> chartsTimeBuilding
+
+    let charts c =
+        c
+        |> Chart.Combine
+        |> Chart.WithLegend(
+            TitleAlignment = Drawing.StringAlignment.Center,
+            Docking = ChartTypes.Docking.Right, 
+            InsideArea = true)
+        |> Chart.WithXAxis(
+            Title = "Количество построенных стаканов",
+            Min = 1.0) 
+        |> Chart.WithYAxis(
+            Title = "Время построения, с",
+            Min = 0.0) 
